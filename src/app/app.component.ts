f0efd7e3273cb9e290c173e607fa2712c194869e
Thomas Woodhouse <thomaswoodhouse93@gmail.com>
2021-02-15 13:24:02 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $Lightning: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'test-app';
+  ngOnInit() {
+    $Lightning.use(
+      'c:ltngOutApp', // name of the Lightning app
+      function () {
+        $Lightning.createComponent(
+          'c:CustomRecordEditForm', // top-level component of your app
+          {}, // attributes to set on the component when created
+          'lightningLocator', // the DOM location to insert the component
+          function (cmp) {
+            // callback when component is created and active on the page
+          }
+        );
+      }
+    );
+  }
 }
